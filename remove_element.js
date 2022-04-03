043e444557607e71f63702eda36b0e9850dbbf58
Daniel Hernandez <danielhernandez918@gmail.com>
2022-04-02 23:04:56 -0700

    update

--- remove_element.js
+++ remove_element.js
@@ -4,15 +4,20 @@ const val1 = 3
 const nums2 = [0,1,2,2,3,0,4,2]
 const val2 = 2
 
+//should take out the value from array and count how many numbers are left after removal of element
 
 var removeElement = function(nums, val) {
-    const newArr= []
+    const number= []
+    let n= 0
     for (let i=0; i<nums.length; i++) {
         if(nums[i]!=val) {
-            newArr.push(nums[i])
+            number.push(nums[i]);
+            nums[n] = nums[i];
+            n++;
         }
     }
-    return newArr
+    console.log(number)
+    return n
 }
 console.log(removeElement(nums1, val1))
 console.log(removeElement(nums2, val2))
